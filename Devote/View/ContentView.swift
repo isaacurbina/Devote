7654b4490ef27a08e48587c77bcfa5fa8c9a0d63
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-05-07 17:52:35 -0500

    124. 3. Let's create a basic to-do app from Xcode's Core Data template (#3)

--- Devote/View/ContentView.swift
+++ Devote/View/ContentView.swift
@@ -13,9 +13,12 @@ struct ContentView: View {
 	
 	// MARK: - property
 	
+	@State var task: String = ""
+	private var isButtonDisabled: Bool {
+		task.isEmpty
+	}
 	// fetching data
 	@Environment(\.managedObjectContext) private var viewContext
-	
 	@FetchRequest(
 		sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
 		animation: .default)
@@ -26,26 +29,68 @@ struct ContentView: View {
 	
 	var body: some View {
 		NavigationView {
-			List {
-				ForEach(items) { item in
-					NavigationLink {
-						Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-					} label: {
-						Text(item.timestamp!, formatter: itemFormatter)
+			VStack{
+				VStack(spacing: 16) {
+					
+					TextField("New Task", text: $task)
+						.padding()
+						.background(Color(UIColor.systemGray6))
+						.cornerRadius(10)
+					
+					Button(action: {
+						addItem()
+					}) {
+						Spacer()
+						Text("SAVE")
+						Spacer()
+					} // Button
+					.disabled(isButtonDisabled)
+					.padding()
+					.font(.headline)
+					.foregroundColor(.white)
+					.background(isButtonDisabled ? .gray : .pink)
+					.cornerRadius(10)
+					
+				} // VStack
+				.padding()
+				
+				List {
+					ForEach(items) { item in
+						NavigationLink {
+							
+							VStack(alignment: .leading) {
+								Text(item.task ?? "")
+									.font(.headline)
+									.fontWeight(.bold)
+								
+								Text("Item at \(item.timestamp!, formatter: itemFormatter)")
+									.font(.footnote)
+									.foregroundColor(.gray)
+							} // VStack
+							
+						} label: {
+							
+							VStack(alignment: .leading) {
+								Text(item.task ?? "")
+									.font(.headline)
+									.fontWeight(.bold)
+								
+								Text(item.timestamp!, formatter: itemFormatter)
+									.font(.footnote)
+									.foregroundColor(.gray)
+							} // VStack
+						}
 					}
-				}
-				.onDelete(perform: deleteItems)
-			} // List
+					.onDelete(perform: deleteItems)
+				} // List
+			} // VStack
+			.navigationBarTitle("Daily Tasks", displayMode: .large)
 			.toolbar {
 				ToolbarItem(placement: .navigationBarLeading) {
 					EditButton()
 				}
-				ToolbarItem(placement: .navigationBarTrailing) {
-					Button(action: addItem) {
-						Label("Add Item", systemImage: "plus")
-					}
-				}
 			} // toolbar
+			
 		} // NavigationView
 	}
 	
@@ -56,15 +101,19 @@ struct ContentView: View {
 		withAnimation {
 			let newItem = Item(context: viewContext)
 			newItem.timestamp = Date()
+			newItem.task = task
+			newItem.completion = false
+			newItem.id = UUID()
 			
 			do {
 				try viewContext.save()
 			} catch {
-				// Replace this implementation with code to handle the error appropriately.
-				// fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
 				let nsError = error as NSError
 				fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
 			}
+			
+			task = ""
+			hideKeyboard()
 		}
 	}
 	
@@ -75,8 +124,6 @@ struct ContentView: View {
 			do {
 				try viewContext.save()
 			} catch {
-				// Replace this implementation with code to handle the error appropriately.
-				// fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
 				let nsError = error as NSError
 				fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
 			}
