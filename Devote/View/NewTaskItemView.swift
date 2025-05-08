c8f9fff41bb0f53ad2bb5da9410797b70080b092
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-05-08 17:04:32 -0500

    126. 5. Create a new entry form for the task item

---
+++ Devote/View/NewTaskItemView.swift
@@ -0,0 +1,103 @@
+//
+//  NewTaskItemView.swift
+//  Devote
+//
+//  Created by Isaac Urbina on 5/8/25.
+//
+
+import SwiftUI
+
+struct NewTaskItemView: View {
+	
+	
+	// MARK: - properties
+	
+	@Environment(\.managedObjectContext) private var viewContext
+	@State private var task: String = ""
+	@Binding var isShowing: Bool
+	private var isButtonDisabled: Bool {
+		task.isEmpty
+	}
+	
+	
+	// MARK: - body
+	
+	var body: some View {
+		VStack {
+			Spacer()
+			
+			VStack(spacing: 16) {
+				
+				TextField("New Task", text: $task)
+					.foregroundColor(.pink)
+					.font(.system(size: 24, weight: .bold, design: .rounded))
+					.padding()
+					.background(Color(UIColor.systemGray6))
+					.cornerRadius(10)
+				
+				Button(action: {
+					addItem()
+				}) {
+					Spacer()
+					
+					Text("SAVE")
+						.font(.system(size: 24, weight: .bold, design: .rounded))
+					
+					Spacer()
+					
+				} // Button
+				.disabled(isButtonDisabled)
+				.padding()
+				.foregroundColor(.white)
+				.background(isButtonDisabled ? .blue : .pink)
+				.cornerRadius(10)
+				
+			} // VStack
+			.padding(.horizontal)
+			.padding(.vertical, 20)
+			.background(.white)
+			.cornerRadius(16)
+			.shadow(
+				color: Color(red: 0, green: 0, blue: 0, opacity: 0.65),
+				radius: 24
+			)
+			.frame(maxWidth: 640)
+			
+		} // VStack
+		.padding()
+	}
+	
+	
+	// MARK: - functions
+	
+	private func addItem() {
+		withAnimation {
+			let newItem = Item(context: viewContext)
+			newItem.timestamp = Date()
+			newItem.task = task
+			newItem.completion = false
+			newItem.id = UUID()
+			
+			do {
+				try viewContext.save()
+			} catch {
+				let nsError = error as NSError
+				fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+			}
+			
+			task = ""
+			hideKeyboard()
+			isShowing = false
+		}
+	}
+}
+
+
+// MARK: - preview
+
+struct NewTaskItemView_Previews: PreviewProvider {
+	static var previews: some View {
+		NewTaskItemView(isShowing: .constant(true))
+			.background(Color.gray.edgesIgnoringSafeArea(.all))
+	}
+}
